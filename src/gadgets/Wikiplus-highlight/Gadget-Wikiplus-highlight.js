0022fc2c7369a48e97067aa1bc771bd85b5a850f
亚衣琴 <Saoutax@gmail.com>
2026-04-18 11:49:12 +0800

    chore: 更新链接

--- src/gadgets/Wikiplus-highlight/Gadget-Wikiplus-highlight.js
+++ src/gadgets/Wikiplus-highlight/Gadget-Wikiplus-highlight.js
@@ -1,6 +1,6 @@
 (async () => {
     const { wgIsArticle, wgAction } = mw.config.get(['wgIsArticle', 'wgAction']);
     if (wgIsArticle && wgAction === 'view') {
-        mw.loader.load('//testingcf.jsdelivr.net/npm/wikiplus-highlight@latest');
+        mw.loader.load('//fastly.jsdelivr.net/npm/wikiplus-highlight@latest');
     }
 })();
