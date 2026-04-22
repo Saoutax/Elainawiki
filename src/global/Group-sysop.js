466f75cac94afd332bba0e1abe391065afb1f481
亚衣琴 <Saoutax@gmail.com>
2026-04-22 13:47:53 +0800

    feat: default import reason

---
+++ src/global/Group-sysop.js
@@ -0,0 +1,11 @@
+$(() => {
+    const { wgCanonicalSpecialPageName } = mw.config.get(['wgCanonicalSpecialPageName']);
+
+    // 默认导入理由
+    if (wgCanonicalSpecialPageName === 'Import') {
+        $('#mw-input-usernamePrefix input').val('zhmoe');
+        $('#mw-input-log-comment input').val(
+            '来源于萌娘百科，依CC BY-NC-SA 3.0 CN导入，原贡献者请参见来源页面历史',
+        );
+    }
+});
