0efb22576d886e9acef578f47085e51bd26bf606
亚衣琴 <Saoutax@gmail.com>
2026-05-23 13:15:42 +0800

    chore: remove unused async

--- src/gadgets/Wikiplus-highlight/Gadget-Wikiplus-highlight.ts
+++ src/gadgets/Wikiplus-highlight/Gadget-Wikiplus-highlight.ts
@@ -1,4 +1,4 @@
-(async () => {
+(() => {
     const { wgIsArticle, wgAction } = mw.config.get(['wgIsArticle', 'wgAction']);
     if (wgIsArticle && wgAction === 'view') {
         mw.loader.load('//fastly.jsdelivr.net/npm/wikiplus-highlight@latest');
