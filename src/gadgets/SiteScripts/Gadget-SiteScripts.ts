55af7aef5f4b0578d0f844a13df2263d41141e17
亚衣琴 <Saoutax@gmail.com>
2026-05-31 10:00:59 +0800

    feat: new gadget

--- src/gadgets/SiteScripts/Gadget-SiteScripts.ts
+++ src/gadgets/SiteScripts/Gadget-SiteScripts.ts
@@ -3,7 +3,7 @@
 
     // 快速填写编辑摘要
     if (wgAction === 'edit') {
-        $('[for="wpSummary"] .mw-summary-preset-item a').on('click', (e) => {
+        $('[for="wpSummary"] .mw-summary-preset-item a').on('click', e => {
             const summaryBox = $('[name="wpSummary"]');
             summaryBox.val(`${String(summaryBox.val())} ${$(e.currentTarget).text()}`.trim());
             summaryBox.trigger('focus');
